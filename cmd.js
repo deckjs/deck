0a8c699e648e12fa108e52e8cfe75abcb84e2554
davidmarkclements <huperekchuno@googlemail.com>
2015-10-23 14:19:30 +0100

    lint

--- cmd.js
+++ cmd.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
 var electron = /electron/i.test(process.argv[0])
-
 if (electron) {
+
   require('./')(require('minimist')(process.argv.slice(2)))
 }
 
 if (!electron) {
+  process.title = 'Deck'
   var spawn = require('child_process').spawn
   spawn(
     require('electron-prebuilt'),
